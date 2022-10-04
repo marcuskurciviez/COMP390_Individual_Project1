864f1c618ad4e3579d1220981ac90e8ab812d4f0
marcuskurciviez <mkurciviez@student.bridgew.edu>
2022-10-03 22:20:15 -0400

    Trying to fix errors

--- meteor_data_class.py
+++ meteor_data_class.py
@@ -1,15 +1,15 @@
 class MeteorDataEntry:
-    def __init__(self, meteor_data, name, id, nametype, recclass, mass, fall, year, reclat, reclong, geolocation, states, counties):
+    def __init__(self, m_name, m_id, m_nametype, m_recclass,m_mass,m_fall, m_year, m_reclat, m_recclong, m_geoloc, m_states, m_counties ):
 
-        self.name = meteor_data[0]
-        self.id = meteor_data[1]
-        self.nametype = meteor_data[2]
-        self.recclass = meteor_data[3]
-        self.mass = meteor_data[4]
-        self.fall = meteor_data[5]
-        self.year = meteor_data[6]
-        self.reclat = meteor_data[7]
-        self.reclong = meteor_data[8]
-        self.geolocation = meteor_data[9]
-        self.states = meteor_data[10]
-        self.counties = meteor_data[11]
+        self.name = m_name[0]
+        self.id = m_id[1]
+        self.nametype = m_nametype[2]
+        self.recclass = m_recclass[3]
+        self.mass = m_mass[4]
+        self.fall = m_fall[5]
+        self.year = m_year[6]
+        self.reclat = m_reclat[7]
+        self.reclong = m_recclong[8]
+        self.geolocation = m_geoloc[9]
+        self.states = m_states[10]
+        self.counties = m_counties[11]
